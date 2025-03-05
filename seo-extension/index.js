285364ffe4e848277623227d0e9af499b238311b
myteam123223 <146390286+myteam123223@users.noreply.github.com>
2025-03-05 20:21:44 +0100

    Update index.js

--- seo-extension/index.js
+++ seo-extension/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'index.css';
-import SEOExtension from './src/lib/SEOExtension';
+import './src/index.css';
+import SEOExtension from './src/SEOExtension';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
